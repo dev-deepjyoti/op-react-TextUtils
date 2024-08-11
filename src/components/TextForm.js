fbe195749a90ea7dcc6ba67f909543b2df2c7613
Deepjyoti Mondal <deepjyoti.mondaldm@gmail.com>
2024-08-11 15:46:12 +0530

    Lesson 7

---
+++ src/components/TextForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("Enter your text here");
+  //   text = "New Text"; - Wrong way to change text
+  //   setText("new Text"); Correct way to set text using function
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="mybox"
+          rows="8"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
+    </div>
+  );
+}
