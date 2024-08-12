5cd05c4a67fa9300e293906f46fc442fdf3063a3
Deepjyoti Mondal <deepjyoti.mondaldm@gmail.com>
2024-08-12 12:10:15 +0530

    lesson 8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text here");
+  const [text, setText] = useState("");
   //   text = "New Text"; - Wrong way to change text
   //   setText("new Text"); Correct way to set text using function
 
@@ -10,24 +10,55 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearText = () => {
+    setText("");
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="mybox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="mybox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-sm btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-sm btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button
+          className="btn btn-sm btn-primary mx-2"
+          onClick={handleClearText}
+          disabled={text === ""}
+        >
+          Clear
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container">
+        <h3 className="my-3">Text Summary</h3>
+        <label>
+          {text === "" ? text.split(" ").length - 1 : text.split(" ").length}{" "}
+          &nbsp;words&nbsp;and&nbsp;
+          {text.length} characters
+        </label>
+        <h4 class="my-2">Preview</h4>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
